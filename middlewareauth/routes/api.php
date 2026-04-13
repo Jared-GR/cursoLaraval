7d14453b86969182630757b371d04146fa3447cb
JaredGR <jared_gutierrez_r@hotmail.com>
2026-04-13 13:05:00 -0600

    middleware v2

--- middlewareauth/routes/api.php
+++ middlewareauth/routes/api.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/create', [AuthController::class, 'createUser']);
 Route::post('/login', [AuthController::class, 'loginUser']);
+
+
+// 1|BuAPgnaX4dmu3FtOQ1si1GV4rGMVxjIMhYPRYvq76e71a52a
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
